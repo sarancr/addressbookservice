879f3499e4616d1e5c878553ee5f309940470316
Saranya Chithalandur Rajendran <sarancr@users.noreply.github.com>
2017-03-27 18:08:25 -0700

    Initial commit

---
+++ src/main/java/com/adb/business/DBProperties.java
@@ -0,0 +1,6 @@
+package com.adb.business;
+
+public interface DBProperties {
+
+	String URL = "jdbc:mysql://aajfc3o6rz2qgy.cschhojukq0p.us-west-1.rds.amazonaws.com:3306/addrbook?" + "user=root&password=root1234";
+}
